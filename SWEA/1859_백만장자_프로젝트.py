1e5698541f89e4cec933586bd2f0abb29e6a3d31
yeoneeds <pink99120@gmail.com>
2022-05-03 23:48:53 +0900

    0503

--- SWEA/1859_백만장자_프로젝트.py
+++ SWEA/1859_백만장자_프로젝트.py
@@ -1,6 +1,16 @@
 t = int(input())
 
-for i in range(t):
+for i in range(1,t+1):
     n = int(input())
-    test_case = list(map(int, input().split()))
+    tc = list(map(int, input().split()))
+    max_sell = tc[-1]
+    max_profit = 0
+    for idx in range(len(tc)-1, -1, -1):
+        if tc[idx]<max_sell:
+            max_profit += max_sell-tc[idx]
+        elif tc[idx]>max_sell:
+            max_sell = tc[idx]
+    print(f"#{i} {max_profit}")
+            
+
     
