e04b33b308d608004a499d3e3919dac23dc0da85
yeoneeds <pink99120@gmail.com>
2022-05-09 23:31:46 +0900

    0509

---
+++ etc/study/재귀/복습.py
@@ -0,0 +1,24 @@
+def atoi(num_str):
+    sign = 1
+    num_lst = list(num_str)
+
+    if num_lst[0]=='-':
+        sign = -1
+        num_lst = num_lst[1:]
+
+#재귀
+def atoi_recur(lst):
+    if not lst:
+        return 0
+    num = int(lst[-1])
+    ret = atoi_recur(lst[:-1])
+    result = num+ret*10
+
+def main():
+    print(atoi("5182"))
+    print(atoi("-281"))
+    print(atoi("0"))
+    print(atoi("-0"))
+
+if __name__ == "__main__":
+    main()
