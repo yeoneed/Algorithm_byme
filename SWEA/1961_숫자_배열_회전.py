3d22ea528d39e751353a90f5535ac69ddb2ae8a0
yeoneeds <pink99120@gmail.com>
2022-05-24 02:19:07 +0900

    1961성공

--- SWEA/1961_숫자_배열_회전.py
+++ SWEA/1961_숫자_배열_회전.py
@@ -14,14 +14,22 @@ def main():
         n = int(input())
         tc = [list(map(int, input().split())) for _ in range(n)]
         result = [[0 for _ in range(n*3)] for _ in range(n)]
+
         print(f"#{t_idx}")
+        
         for solve_num in range(3):
             tc_tmp = [[0 for _ in range(n)] for _ in range(n)]
             tc = solve(n, tc, tc_tmp)
             for i in range(n):
                 for j in range(n):
-                    for k in range(solve_num, solve_num+n):
-                        result[i][j] = 
+                    result[i][j+(n*solve_num)] = tc[i][j]
+
+        for i in range(n):
+            for j in range(n*3):
+                print(result[i][j], end='')
+                if j>0 and j%n==n-1:
+                    print('', end = ' ')
+            print()
 
 
 if __name__=="__main__":
