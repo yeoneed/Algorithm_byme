9e1f349aa0d66cb2e3df6e97133363f89e1a932d
yeoneeds <pink99120@gmail.com>
2022-05-29 02:45:48 +0900

    0528

--- etc/study/재귀/BFS/BFS_복습3.py
+++ etc/study/재귀/BFS/BFS_복습3.py
@@ -1,34 +1,35 @@
+#length.py ((0, 0)부터 (n-1, m-1)까지의 최단 경로 길이 구하기)
+
 from collections import deque
 
-dx = [-1,1,0,0]
+dx = [-1,1,0,0] #상 하 좌 우
 dy = [0,0,-1,1]
 
 def is_valid_xy(n,m,x,y):
     return x>=0 and x<n and y>=0 and y<m
 
-def bfs(n,m,board, visited, src_x, src_y, dst_x, dst_y):
-    size = 0
-    q = deque()
-    q.append((src_x,src_y,0)) #출발지 깊이는 0
-    visited[x][y] =1
-    while q:
-        x,y,l = q.popleft()
-        size+=1
-        if (x, y) == (dst_x, dst_y):
-            return l
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if is_valid_xy(n,m,nx,ny)==1 and visited[nx][ny]!=1 and board[nx][ny]==1:
-                q.append((nx,ny, l+1))
-                visited[nx][ny]=1
-    return -1
-
 def solve(n, m, board):
     visited = [[0 for _ in range(m)] for _ in range(n)]
-    return  bfs(n,m,board, visited,0,0,n-1,m-1)
-    
+    length = bfs(n,m,board, visited, 0,0, n-1, m-1)
+    return length
 
+def bfs(n,m,board,visited,start_x, start_y, dst_x, dst_y):
+    q = deque()
+    q.append((start_x, start_y, 0))
+    visited[start_x][start_y] =1
+    while q:
+        x,y,l = q.popleft()
+        if (x,y)== (dst_x, dst_y): #목표 좌표에 도착 시 처음부터의 해당 좌표까지의 길이 반환
+            return l
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if is_valid_xy(n,m,nx,ny)==1 and board[nx][ny]==1 and visited[nx][ny]==0:
+                q.append((nx, ny, l+1))
+                visited[nx][ny] =1
+    return -1
+
+def history()
 def main():
   # case 1
   n = 15
@@ -51,7 +52,6 @@ def main():
     '11100011111111'
   ]
   board = [list(map(int, row)) for row in board]
-
   length = solve(n, m, board)
   print(length)
 
@@ -61,9 +61,9 @@ def main():
   board = [
     '11000',
     '10001',
-    '10101',
+    '00101',
     '10001',
-    '11111'
+    '10111'
   ]
   board = [list(map(int, row)) for row in board]
   length = solve(n, m, board)
