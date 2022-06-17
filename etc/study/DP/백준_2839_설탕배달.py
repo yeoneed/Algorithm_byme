c570e3308b40606b7d9618f77a5de0885096aa86
yeoneeds <pink99120@gmail.com>
2022-06-17 17:07:53 +0900

    0617

--- etc/study/DP/백준_2839_설탕배달.py
+++ etc/study/DP/백준_2839_설탕배달.py
@@ -4,17 +4,25 @@ read_ints = lambda: list(map(int, sys.stdin.readline().strip().split()))
 read_int = lambda: read_ints()[0]
 
 def dp(n, save):
-    for i in range(6, n+1):
-        save[i] = min(save[i-3], save[i-5]) +1
+    if n==3 or n==5:
+        return 1
+    for i in range(4, n+1):
+        if save[i-3]!=-1 and save[i-5]!=-1:
+            save[i] = min(save[i-3], save[i-5]) +1
+        elif save[i-3]==-1 and save[i-5]!=-1:
+            save[i] = save[i-5]+1
+        elif save[i-3]!=-1 and save[i-5]==-1:
+            save[i] = save[i-3]+1
+    
     return save[n]
+
 def main():
     n = read_int()
     save = [-1] * (n+1)
     save[3] = 1
-    save[5] = 1
-    save[4] = -1
+    if n>=5:
+        save[5]=1
     print(dp(n,save))
-
 
 if __name__ == "__main__":
     main()
