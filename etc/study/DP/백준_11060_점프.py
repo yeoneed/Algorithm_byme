112f9ba3df043e6204fcc07f6cc5aa0900ab5cb6
yeoneeds <pink99120@gmail.com>
2022-06-20 23:12:29 +0900

    0620

---
+++ etc/study/DP/백준_11060_점프.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.stdin = open("input.txt")
+
+read_ints = lambda: list(map(int, sys.stdin.readline().strip().split()))
+read_int = lambda: read_ints()[0]
+
+def dp(n,lst):
+    cnt = 0
+    for i in range(n):
+        for j in range(i,lst[i]+1):
+            if i+j<n:
+                
+
+
+def main():
+    n = read_int()
+    lst = read_ints()
+    print(dp(n,lst))
+    
+if __name__ == "__main__":
+    main()
