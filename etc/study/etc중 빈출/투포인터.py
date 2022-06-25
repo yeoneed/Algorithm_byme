7580f0bfd8b3b9fc1209182ca9fff014102d4cc6
yeoneed <pink99120@gmail.com>
2022-06-26 00:17:01 +0900

    0625_twopointer

--- etc/study/etc중 빈출/투포인터.py
+++ etc/study/etc중 빈출/투포인터.py
@@ -1,9 +1,13 @@
 #합병 정렬
 import sys
 
-sys.stdin = open("input.txt")
+#read_ints = lambda: list(map(int, sys.stdin.readline().strip().split()))
+#read_int = lambda: read_ints()[0]
 
 def print_array(lst):
+    for i in lst:
+        print(i, end = ' ')
+    print()
 
 def merge_sort(lst):
     if len(lst)==1:
@@ -13,9 +17,15 @@ def merge_sort(lst):
     right = merge_sort(lst[mid:])
 
     new = [0 for _ in range(len(lst))]
+    l = 0
+    r = 0
     for i in range(len(lst)):
-        l = 0
-        r = 0
+        if r>=len(right):
+            new[i:] = left[l:]
+            return new
+        elif l>=len(left):
+            new[i:] = right[r:]
+            return new
         if left[l]<right[r]:
             new[i]= left[l]
             l+=1
@@ -23,13 +33,11 @@ def merge_sort(lst):
             new[i]= right[r]
             r+=1
 
-
-
 def main():
     array = list(reversed(range(10)))
-	print_array(array)
-	array = merge_sort(array)
-	print_array(array)
+    print_array(array)
+    array = merge_sort(array)
+    print_array(array)
 
 
 if __name__ == "__main__":
